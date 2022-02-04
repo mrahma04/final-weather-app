4ca70ab8aedc6cfe5ca5d26004accd92b450a7ac
Mohammad Rahman <mbrahman@gmail.com>
2022-02-04 07:42:34 -0500

    finished adding color logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,9 @@ var cityFormEl = document.querySelector('#city-form')
 
 var todaysWeatherEl = document.querySelector('#todays-weather')
 
-var dayOneEl = document.querySelector('#day-one')
+// var dayOneEl = document.querySelector('#day-one')
+var forecastSectionEl = document.querySelector('#weather-section')
+var forecastRowEl = document.querySelector('#forecast-row')
 
 var apiKey = 'a08a76e0b432a1339ec48baaa41272b8'
 
@@ -30,23 +32,61 @@ var getWeather = function (city) {
 }
 
 var renderTodaysWeather = function (weather) {
-    console.log(weather)
+
+    todaysWeatherEl.textContent = ''
 
     var todaysHeader = document.createElement('h5')
     todaysHeader.textContent = weather.city.name
 
-    todaysWeatherEl.append(todaysHeader)
+    var temp = weather.list[0].main.temp
+    var tempEl = document.createElement('p')
+    tempEl.textContent = 'Temp: ' + temp + " \u2109"
+
+    var windSpeed = weather.list[0].wind.speed
+    var windEl = document.createElement('p')
+    windEl.textContent = 'Wind: ' + windSpeed + ' MPH'
+
+    var humidity = weather.list[0].main.humidity
+    var humidityEl = document.createElement('p')
+    humidityEl.textContent = 'Humidity: ' + humidity + '%'
+
+    if (temp < 35) {
+        todaysWeatherEl.style.backgroundColor = '#94d2bd'
+    } else {
+        todaysWeatherEl.style.backgroundColor = '#ee9b00'
+    }
+
+    todaysWeatherEl.classList.add('border', 'border-dark')
+    todaysWeatherEl.append(todaysHeader, tempEl, windEl, humidityEl)
 
 }
 
 var renderForecast = function (forecast) {
-    console.log(forecast)
 
-    var tempEl = document.createElement('p')
-    tempEl.textContent = 'Temp' + ': ' + forecast.list[1].main.temp
+    forecastRowEl.textContent = ''
 
-    dayOneEl.append(tempEl)
+    for (let i = 1; i < forecast.list.length; i++) {
+        var parentDivEl = document.createElement('div')
+        parentDivEl.classList.add('col')
 
+        var childDivEl = document.createElement('div')
+        childDivEl.classList.add('card', 'text-white', 'bg-dark')
+        childDivEl.setAttribute('style', 'max-width: 12rem; min-height: 10rem')
+
+        var tempEl = document.createElement('p')
+        tempEl.textContent = 'Temp: ' + forecast.list[i].main.temp + " \u2109"
+
+        var windEl = document.createElement('p')
+        windEl.textContent = 'Wind: ' + forecast.list[i].wind.speed + ' MPH'
+
+        var humidityEl = document.createElement('p')
+        humidityEl.textContent = 'Humidity: ' + forecast.list[i].main.humidity + '%'
+
+        childDivEl.append(tempEl, windEl, humidityEl)
+        parentDivEl.append(childDivEl)
+
+        forecastRowEl.append(parentDivEl)
+    }
 }
 
 cityFormEl.addEventListener('submit', formSubmitHandler)
